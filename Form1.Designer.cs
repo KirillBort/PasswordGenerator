fb1354db03c7b524321153c7184a6bb708d4b7b4
Degt_ya91@student.itstep.org <Degt_ya91@student.itstep.org>
2021-12-26 12:12:17 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace PasswordGenerator
+{
+    partial class PasswordGeneratorForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordLengthNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.PasswordResultTextBox = new System.Windows.Forms.TextBox();
+            this.GeneratePasswordButton = new System.Windows.Forms.Button();
+            this.EnglishLettersCheckBox = new System.Windows.Forms.CheckBox();
+            this.CapitalEnglishLettersCheckBox = new System.Windows.Forms.CheckBox();
+            this.RussianLettersCheckBox = new System.Windows.Forms.CheckBox();
+            this.CapitalRussianLettersCheckBox = new System.Windows.Forms.CheckBox();
+            this.NumbersCheckBox = new System.Windows.Forms.CheckBox();
+            this.SpecialSymbolsCheckBox = new System.Windows.Forms.CheckBox();
+            this.SelectPasswordSizeLabel = new System.Windows.Forms.Label();
+            this.SelectPasswordOptionsLabel = new System.Windows.Forms.Label();
+            this.GenerationResultLabel = new System.Windows.Forms.Label();
+            this.CopyPasswordButton = new System.Windows.Forms.Button();
+            this.AllOptionsCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordLengthNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PasswordLengthNumericUpDown
+            // 
+            this.PasswordLengthNumericUpDown.Location = new System.Drawing.Point(34, 82);
+            this.PasswordLengthNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PasswordLengthNumericUpDown.Name = "PasswordLengthNumericUpDown";
+            this.PasswordLengthNumericUpDown.Size = new System.Drawing.Size(135, 20);
+            this.PasswordLengthNumericUpDown.TabIndex = 0;
+            this.PasswordLengthNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // PasswordResultTextBox
+            // 
+            this.PasswordResultTextBox.Location = new System.Drawing.Point(313, 82);
+            this.PasswordResultTextBox.Name = "PasswordResultTextBox";
+            this.PasswordResultTextBox.ReadOnly = true;
+            this.PasswordResultTextBox.Size = new System.Drawing.Size(670, 20);
+            this.PasswordResultTextBox.TabIndex = 1;
+            // 
+            // GeneratePasswordButton
+            // 
+            this.GeneratePasswordButton.Location = new System.Drawing.Point(734, 333);
+            this.GeneratePasswordButton.Name = "GeneratePasswordButton";
+            this.GeneratePasswordButton.Size = new System.Drawing.Size(120, 23);
+            this.GeneratePasswordButton.TabIndex = 2;
+            this.GeneratePasswordButton.Text = "Generate Password";
+            this.GeneratePasswordButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordButton.Click += new System.EventHandler(this.GeneratePasswordButton_Click);
+            // 
+            // EnglishLettersCheckBox
+            // 
+            this.EnglishLettersCheckBox.AutoSize = true;
+            this.EnglishLettersCheckBox.Location = new System.Drawing.Point(34, 189);
+            this.EnglishLettersCheckBox.Name = "EnglishLettersCheckBox";
+            this.EnglishLettersCheckBox.Size = new System.Drawing.Size(95, 17);
+            this.EnglishLettersCheckBox.TabIndex = 3;
+            this.EnglishLettersCheckBox.Text = "English Letters";
+            this.EnglishLettersCheckBox.UseVisualStyleBackColor = true;
+            this.EnglishLettersCheckBox.Click += new System.EventHandler(this.EnglishLettersCheckBox_Click);
+            // 
+            // CapitalEnglishLettersCheckBox
+            // 
+            this.CapitalEnglishLettersCheckBox.AutoSize = true;
+            this.CapitalEnglishLettersCheckBox.Location = new System.Drawing.Point(34, 213);
+            this.CapitalEnglishLettersCheckBox.Name = "CapitalEnglishLettersCheckBox";
+            this.CapitalEnglishLettersCheckBox.Size = new System.Drawing.Size(130, 17);
+            this.CapitalEnglishLettersCheckBox.TabIndex = 4;
+            this.CapitalEnglishLettersCheckBox.Text = "Capital English Letters";
+            this.CapitalEnglishLettersCheckBox.UseVisualStyleBackColor = true;
+            this.CapitalEnglishLettersCheckBox.Click += new System.EventHandler(this.CapitalEnglishLettersCheckBox_Click);
+            // 
+            // RussianLettersCheckBox
+            // 
+            this.RussianLettersCheckBox.AutoSize = true;
+            this.RussianLettersCheckBox.Location = new System.Drawing.Point(34, 236);
+            this.RussianLettersCheckBox.Name = "RussianLettersCheckBox";
+            this.RussianLettersCheckBox.Size = new System.Drawing.Size(99, 17);
+            this.RussianLettersCheckBox.TabIndex = 5;
+            this.RussianLettersCheckBox.Text = "Russian Letters";
+            this.RussianLettersCheckBox.UseVisualStyleBackColor = true;
+            this.RussianLettersCheckBox.Click += new System.EventHandler(this.RussianLettersCheckBox_Click);
+            // 
+            // CapitalRussianLettersCheckBox
+            // 
+            this.CapitalRussianLettersCheckBox.AutoSize = true;
+            this.CapitalRussianLettersCheckBox.Location = new System.Drawing.Point(34, 260);
+            this.CapitalRussianLettersCheckBox.Name = "CapitalRussianLettersCheckBox";
+            this.CapitalRussianLettersCheckBox.Size = new System.Drawing.Size(134, 17);
+            this.CapitalRussianLettersCheckBox.TabIndex = 6;
+            this.CapitalRussianLettersCheckBox.Text = "Capital Russian Letters";
+            this.CapitalRussianLettersCheckBox.UseVisualStyleBackColor = true;
+            this.CapitalRussianLettersCheckBox.Click += new System.EventHandler(this.CapitalRussianLettersCheckBox_Click);
+            // 
+            // NumbersCheckBox
+            // 
+            this.NumbersCheckBox.AutoSize = true;
+            this.NumbersCheckBox.Location = new System.Drawing.Point(34, 283);
+            this.NumbersCheckBox.Name = "NumbersCheckBox";
+            this.NumbersCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.NumbersCheckBox.TabIndex = 8;
+            this.NumbersCheckBox.Text = "Numbers";
+            this.NumbersCheckBox.UseVisualStyleBackColor = true;
+            this.NumbersCheckBox.Click += new System.EventHandler(this.NumbersCheckBox_Click);
+            // 
+            // SpecialSymbolsCheckBox
+            // 
+            this.SpecialSymbolsCheckBox.AutoSize = true;
+            this.SpecialSymbolsCheckBox.Location = new System.Drawing.Point(34, 307);
+            this.SpecialSymbolsCheckBox.Name = "SpecialSymbolsCheckBox";
+            this.SpecialSymbolsCheckBox.Size = new System.Drawing.Size(103, 17);
+            this.SpecialSymbolsCheckBox.TabIndex = 9;
+            this.SpecialSymbolsCheckBox.Text = "Special Symbols";
+            this.SpecialSymbolsCheckBox.UseVisualStyleBackColor = true;
+            this.SpecialSymbolsCheckBox.Click += new System.EventHandler(this.SpecialSymbolsCheckBox_Click);
+            // 
+            // SelectPasswordSizeLabel
+            // 
+            this.SelectPasswordSizeLabel.AutoSize = true;
+            this.SelectPasswordSizeLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectPasswordSizeLabel.Location = new System.Drawing.Point(30, 63);
+            this.SelectPasswordSizeLabel.Name = "SelectPasswordSizeLabel";
+            this.SelectPasswordSizeLabel.Size = new System.Drawing.Size(139, 19);
+            this.SelectPasswordSizeLabel.TabIndex = 10;
+            this.SelectPasswordSizeLabel.Text = "Select Password Size";
+            // 
+            // SelectPasswordOptionsLabel
+            // 
+            this.SelectPasswordOptionsLabel.AutoSize = true;
+            this.SelectPasswordOptionsLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectPasswordOptionsLabel.Location = new System.Drawing.Point(30, 167);
+            this.SelectPasswordOptionsLabel.Name = "SelectPasswordOptionsLabel";
+            this.SelectPasswordOptionsLabel.Size = new System.Drawing.Size(162, 19);
+            this.SelectPasswordOptionsLabel.TabIndex = 11;
+            this.SelectPasswordOptionsLabel.Text = "Select Password Options";
+            // 
+            // GenerationResultLabel
+            // 
+            this.GenerationResultLabel.AutoSize = true;
+            this.GenerationResultLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerationResultLabel.Location = new System.Drawing.Point(313, 63);
+            this.GenerationResultLabel.Name = "GenerationResultLabel";
+            this.GenerationResultLabel.Size = new System.Drawing.Size(116, 19);
+            this.GenerationResultLabel.TabIndex = 12;
+            this.GenerationResultLabel.Text = "Generation Result";
+            // 
+            // CopyPasswordButton
+            // 
+            this.CopyPasswordButton.Location = new System.Drawing.Point(889, 333);
+            this.CopyPasswordButton.Name = "CopyPasswordButton";
+            this.CopyPasswordButton.Size = new System.Drawing.Size(94, 23);
+            this.CopyPasswordButton.TabIndex = 13;
+            this.CopyPasswordButton.Text = "Copy Password";
+            this.CopyPasswordButton.UseVisualStyleBackColor = true;
+            this.CopyPasswordButton.Click += new System.EventHandler(this.CopyPasswordButton_Click);
+            // 
+            // AllOptionsCheckBox
+            // 
+            this.AllOptionsCheckBox.AutoSize = true;
+            this.AllOptionsCheckBox.Location = new System.Drawing.Point(34, 330);
+            this.AllOptionsCheckBox.Name = "AllOptionsCheckBox";
+            this.AllOptionsCheckBox.Size = new System.Drawing.Size(76, 17);
+            this.AllOptionsCheckBox.TabIndex = 14;
+            this.AllOptionsCheckBox.Text = "All Options";
+            this.AllOptionsCheckBox.UseVisualStyleBackColor = true;
+            this.AllOptionsCheckBox.CheckedChanged += new System.EventHandler(this.AllOptionsCheckBox_CheckedChanged);
+            // 
+            // PasswordGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 383);
+            this.Controls.Add(this.AllOptionsCheckBox);
+            this.Controls.Add(this.CopyPasswordButton);
+            this.Controls.Add(this.GenerationResultLabel);
+            this.Controls.Add(this.SelectPasswordOptionsLabel);
+            this.Controls.Add(this.SelectPasswordSizeLabel);
+            this.Controls.Add(this.SpecialSymbolsCheckBox);
+            this.Controls.Add(this.NumbersCheckBox);
+            this.Controls.Add(this.CapitalRussianLettersCheckBox);
+            this.Controls.Add(this.RussianLettersCheckBox);
+            this.Controls.Add(this.CapitalEnglishLettersCheckBox);
+            this.Controls.Add(this.EnglishLettersCheckBox);
+            this.Controls.Add(this.GeneratePasswordButton);
+            this.Controls.Add(this.PasswordResultTextBox);
+            this.Controls.Add(this.PasswordLengthNumericUpDown);
+            this.Name = "PasswordGeneratorForm";
+            this.Text = "PasswordGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordLengthNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown PasswordLengthNumericUpDown;
+        private System.Windows.Forms.TextBox PasswordResultTextBox;
+        private System.Windows.Forms.Button GeneratePasswordButton;
+        private System.Windows.Forms.CheckBox EnglishLettersCheckBox;
+        private System.Windows.Forms.CheckBox CapitalEnglishLettersCheckBox;
+        private System.Windows.Forms.CheckBox RussianLettersCheckBox;
+        private System.Windows.Forms.CheckBox CapitalRussianLettersCheckBox;
+        private System.Windows.Forms.CheckBox NumbersCheckBox;
+        private System.Windows.Forms.CheckBox SpecialSymbolsCheckBox;
+        private System.Windows.Forms.Label SelectPasswordSizeLabel;
+        private System.Windows.Forms.Label SelectPasswordOptionsLabel;
+        private System.Windows.Forms.Label GenerationResultLabel;
+        private System.Windows.Forms.Button CopyPasswordButton;
+        private System.Windows.Forms.CheckBox AllOptionsCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class PasswordGeneratorForm : Form
+    {
+        public PasswordGeneratorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, EventArgs e)
+        {
+            if (EnglishLettersCheckBox.Checked | CapitalEnglishLettersCheckBox.Checked | RussianLettersCheckBox.Checked | CapitalRussianLettersCheckBox.Checked | NumbersCheckBox.Checked | SpecialSymbolsCheckBox.Checked | AllOptionsCheckBox.Checked != false)
+            {
+                var Password = new StringBuilder();
+                var random = new Random();
+                var RussianLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+                var CapitalRussianLetters = RussianLetters.ToUpper();
+                var SpecialSymbols = "!@#$%^&*()[]|";
+                for (int i = 0; i < PasswordLengthNumericUpDown.Value;)
+                {
+                    switch (random.Next(0, 6))
+                    {
+                        case 0:
+                            if (EnglishLettersCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(Convert.ToChar(random.Next(97, 123)));
+                                i++;
+                            }
+                            break;
+                        case 1:
+                            if (CapitalEnglishLettersCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(Convert.ToChar(random.Next(65, 91)));
+                                i++;
+                            }
+                            break;
+                        case 2:
+                            if (RussianLettersCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(RussianLetters[random.Next(RussianLetters.Length)]);
+                                i++;
+                            }
+                            break;
+                        case 3:
+                            if (CapitalRussianLettersCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(CapitalRussianLetters[random.Next(CapitalRussianLetters.Length)]);
+                                i++;
+                            }
+                            break;
+                        case 4:
+                            if (NumbersCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(random.Next(0, 10).ToString();
+                                i++;
+                            }
+                            break;
+                        case 5:
+                            if (SpecialSymbolsCheckBox.Checked || AllOptionsCheckBox.Checked)
+                            {
+                                Password.Append(SpecialSymbols[random.Next(SpecialSymbols.Length)]);
+                                i++;
+                            }
+                            break;
+                    }
+                }
+                PasswordResultTextBox.Text = Password.ToString();
+            }
+            else
+            {
+                MessageBox.Show("YOU DON'T CHOOSE ANY OPTION!", "OPTIONS DON'T CHOOSED", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CopyPasswordButton_Click(object sender, EventArgs e)
+        {
+            if (PasswordResultTextBox.Text.Length != 0)
+            {
+                Clipboard.SetText(PasswordResultTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("YOU DON'T GENERATE PASSWORD YET!", "PASSWORD DON`T GENERATED", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void AllOptionsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            var CheckedState = !(sender as CheckBox).Checked;
+            if (!CheckedState)
+            {
+                EnglishLettersCheckBox.Checked = CheckedState;
+                CapitalEnglishLettersCheckBox.Checked = CheckedState;
+                RussianLettersCheckBox.Checked = CheckedState;
+                CapitalRussianLettersCheckBox.Checked = CheckedState;
+                NumbersCheckBox.Checked = CheckedState;
+                SpecialSymbolsCheckBox.Checked = CheckedState;
+            }
+
+        }
+        private void EnglishLettersCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+        private void CapitalEnglishLettersCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+        private void RussianLettersCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+        private void CapitalRussianLettersCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+        private void NumbersCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+        private void SpecialSymbolsCheckBox_Click(object sender, EventArgs e)
+        {
+            AllOptionsCheckBox.Checked = false;
+        }
+    }
+}
