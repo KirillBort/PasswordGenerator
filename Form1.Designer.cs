e8669d01fa93c033dfc70f0518ac182a9aaa2b45
OLECSII MIR <zachhytnyk@live.com>
2021-12-26 12:30:59 +0200

    Check

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -201,7 +201,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(298, 167);
+            this.linkLabel1.Location = new System.Drawing.Point(31, 371);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(996, 13);
             this.linkLabel1.TabIndex = 15;
